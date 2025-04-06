778be63c7d584407a22f8a58855bb8b756b90cfe
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-04-06 11:27:47 -0400

    config: enable https with local certs to access location api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import fs from "node:fs";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		https: {
+			key: fs.readFileSync("certs/key.pem"),
+			cert: fs.readFileSync("certs/cert.pem"),
+		},
+	},
+});
