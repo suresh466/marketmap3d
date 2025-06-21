47324f982219f3b54209c98e151ae6f53958687f
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-06-21 13:44:10 -0400

    feat: add converted floor plan GeoJSON data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,22 +6,22 @@ import { loadGraphmlAsGeoJson } from "./utils";
 
 function App() {
 
-  useEffect(() => {
-    loadGraphmlAsGeoJson("../fleamarket_floormap_scaled.graphml")
-      .then((data) => {
-        // Save GeoJSON output as a file
-        const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "floorPlanData.geojson";
-        // link.click();
-        URL.revokeObjectURL(url);
-      })
-      .catch((error) => {
-        console.error("Error loading floor plan:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   loadGraphmlAsGeoJson("../fleamarket_floormap_scaled.graphml")
+  //     .then((data) => {
+  //       // Save GeoJSON output as a file
+  //       const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+  //       const url = URL.createObjectURL(blob);
+  //       const link = document.createElement("a");
+  //       link.href = url;
+  //       link.download = "floorPlanData.geojson";
+  //       // link.click();
+  //       URL.revokeObjectURL(url);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error loading floor plan:", error);
+  //     });
+  // }, []);
 
 
   return (
