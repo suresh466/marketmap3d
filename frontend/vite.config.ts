0215bbb75c588205baa13433e9b3efbbeb4c616b
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-09-21 15:30:29 -0400

    config: tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -8,5 +9,6 @@ export default defineConfig({
 				plugins: ["babel-plugin-react-compiler"],
 			},
 		}),
+		tailwindcss(),
 	],
 });
